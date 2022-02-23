3d476f286b898ce189bab3f92275fdd284393046
IsmailAtaev <76068787+IsmailAtaev@users.noreply.github.com>
2022-02-24 02:48:30 +0300

    fix

--- lab2.py
+++ lab2.py
@@ -2,4 +2,4 @@ import math
 x = float(input("input x = "))
 y = float(input("input y = "))
 f = (((math.log(x-y)) / (x-y) * (pow(x, 4) + pow(y, 4)) + (4 * pow(x, 2) * pow(y, 2))) * math.log1p(math.sqrt(1 + pow(x, 2) + pow(y, 2))))
-print(f)
+print("Answer: ", f)
