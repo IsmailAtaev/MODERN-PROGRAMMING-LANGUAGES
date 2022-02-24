64d47e2fcd470e0d44b6a7ef3e7ac75ca603f84a
IsmailAtaev <76068787+IsmailAtaev@users.noreply.github.com>
2022-02-24 13:20:34 +0300

    fixed pow (**)

--- lab2.py
+++ lab2.py
@@ -1,5 +1,12 @@
-import math
+"""import math
 x = float(input("input x = "))
 y = float(input("input y = "))
 f = (((math.log(x-y)) / (x-y) * (pow(x, 4) + pow(y, 4)) + (4 * pow(x, 2) * pow(y, 2))) * math.log1p(math.sqrt(1 + pow(x, 2) + pow(y, 2))))
-print("Answer: ", f)
+print("Answer: ", f)"""
+
+
+from math import log, sqrt
+x = float(input("input x = "))
+y = float(input("input y = "))
+f = (((log(x-y)) / (x-y) * (x**4 + y**4) + (4 * x**2 * y**2)) * log(sqrt(1 + x**2 + y**2)))
+print('Answer: ', f)
