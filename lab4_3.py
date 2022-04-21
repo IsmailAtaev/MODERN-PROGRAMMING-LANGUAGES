f1d8cbb94134db8be6f78e2e3e567e2567f2fc8d
IsmailAtaev <76068787+IsmailAtaev@users.noreply.github.com>
2022-04-21 16:07:05 +0300

    fix: fixed lab 4 task 3

--- lab4_3.py
+++ lab4_3.py
@@ -1,11 +1,23 @@
 dict_eng = {'white': 'белый', 'yellow': 'желтый', 'red': 'красный', 'green': 'зелёный', 'blue': 'синий',
-'table': 'стол', 'window': 'окно', 'pen': 'ручка', 'bed': 'кровать', 'pencil': 'карандаш',
-'turkey': 'индюк', 'cat': 'кошка', 'dog': 'собака', 'cow': 'корова', 'brother': 'брат'}
+            'table': 'стол', 'window': 'окно', 'pen': 'ручка', 'bed': 'кровать', 'pencil': 'карандаш',
+            'turkey': 'индюк', 'cat': 'кошка', 'dog': 'собака', 'cow': 'корова', 'brother': 'брат',
+            'phone': 'телефон'}
+
+my_list = []
+
 while True:
-    s = input()
+    s = input('Введите слово\n\t')
     print(dict_eng[s])
+    my_list.append(dict_eng[s])
     b = int(input('Хотите повторить: 1:да , 2:нет '))
     if b == 1:
         continue
     else:
         break
+
+my_list.sort()
+print(my_list)
+
+#new_list = sorted(dict_eng.items(), key=lambda x: x[1])
+#sort_values = dict(new_list)
+#print(sort_values)
